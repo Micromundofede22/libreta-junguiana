e586a0829fbbad613d8917746aad61e412591bdc
Fede Folmer <fedefolmer00@gmail.com>
2024-03-13 08:26:03 -0300

    add fileFilter in object multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,30 +1,47 @@
 import multer from "multer";
 import { __dirname } from "../utils.js";
-import path from "path"
+import path from "path";
+import { error } from "console";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     if (file.fieldname === "dream_image") {
       cb(null, __dirname + "/public/dreams");
-    };
-    if(file.fieldname === "image_synchro"){
-        cb(null, __dirname + "/public/synchronicities");
-    };
-    if(file.fieldname === "dni"){
-        cb(null, __dirname + "/public/dni"); //solicitar al psicólogo
-    };
-    if(file.fieldname === "curriculum"){
-        cb(null, __dirname + "/public/curriculum"); //solicitar al psicólogo
-    };
-    if(file.fieldname === "imageProfile"){
-        cb(null, __dirname + "/public/profile"); 
-    };
+    }
+    if (file.fieldname === "image_synchro") {
+      cb(null, __dirname + "/public/synchronicities");
+    }
+    if (file.fieldname === "dni") {
+      cb(null, __dirname + "/public/dni"); //solicitar al psicólogo
+    }
+    if (file.fieldname === "curriculum") {
+      cb(null, __dirname + "/public/curriculum"); //solicitar al psicólogo
+    }
+    if (file.fieldname === "imageProfile") {
+      cb(null, __dirname + "/public/profile");
+    }
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext= path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
     cb(null, file.fieldname + "-" + uniqueSuffix + ext);
   },
 });
 
-export const uploader = multer({ storage });
+export const uploader = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if(file.fieldname === "imageProfile"){
+      if (path.extname(file.originalname) !== (".png" && ".jpg")) {
+        return cb(new Error("Extensión incompatible. Cargue archivo .png o .jpg"));
+      };
+      cb(null, true);
+    };
+    if(file.fieldname === "curriculum"){
+      if (path.extname(file.originalname) !== ".pdf") {
+        return cb(new Error("Extensión incompatible. Cargue archivo .pdf"));
+      };
+      cb(null, true);
+    };
+  },
+});
